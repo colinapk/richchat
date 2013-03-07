fccc08bf373de6a4bda737e3e789140ef6a1944a
colinapk <china.jiawang@gmail.com>
2013-03-07 15:34:28 +0800

    Initial Commit

---
+++ richchat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  richchat
+//
+//  Created by jia wang on 3/7/13.
+//  Copyright (c) 2013 Colin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ richchat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  richchat
+//
+//  Created by jia wang on 3/7/13.
+//  Copyright (c) 2013 Colin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
